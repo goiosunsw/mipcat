9895f50a23725db3a29221cff1adb862f96f5907
goiosunsw <a.almeida@unsw.edu.au>
2022-09-27 15:57:01 +1000

    add missing modules

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ setup(name='mipcat',
       version='0.1',
       description='Scripts for the analysis of emotion in music recordings',
       url='http://github.com/goiosunw/mipcat',
-      author='Andre Goios',
+      author='Andre Almeida',
       author_email='a.almeida@unsw.edu.au',
       license='GPL v3',
-      packages=['mipcat.signal', 'mipcat.video'],
+      packages=['mipcat', 'mipcat.signal', 'mipcat.video', 'mipcat.align'],
       test_suite = 'setup.my_tests',
       zip_safe=False)
 
